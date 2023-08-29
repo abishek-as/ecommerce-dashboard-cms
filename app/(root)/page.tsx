d37af8dbfe9453624130c18847c8f9c7c6ae1769
abishek-as <abeshek.as@gmail.com>
2023-08-29 12:42:39 +0530

    Clerk auth and Modal components (Admin)

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,20 @@
-import { Button } from "@/components/ui/button";
+"use client";
 
-export default function Home() {
-	return (
-		<div>
-			<Button>Click Me</Button>
-		</div>
-	);
-}
+import { useEffect } from "react";
+
+import { useStoreModal } from "@/hooks/use-store-modal";
+
+const SetupPage = () => {
+	const onOpen = useStoreModal((state) => state.onOpen);
+	const isOpen = useStoreModal((state) => state.isOpen);
+
+	useEffect(() => {
+		if (!isOpen) {
+			onOpen();
+		}
+	}, [isOpen, onOpen]);
+
+	return <div className="p-4">Root Page</div>;
+};
+
+export default SetupPage;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ModalProvider } from "@/providers/modal-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +17,14 @@ export default function RootLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<html lang="en">
-			<body className={inter.className}>{children}</body>
-		</html>
+		<ClerkProvider>
+			<html lang="en">
+				<body className={inter.className}>
+					{" "}
+					<ModalProvider />
+					{children}
+				</body>
+			</html>
+		</ClerkProvider>
 	);
 }

bea5b258430a2daf70e6e454585ec5e254037d96
abishek-as <abeshek.as@gmail.com>
2023-08-31 23:50:16 +0530

    prisma, planetscale, mysql setup (Admin)

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ModalProvider } from "@/providers/modal-provider";
+import { ToasterProvider } from "@/providers/toast-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +21,7 @@ export default function RootLayout({
 		<ClerkProvider>
 			<html lang="en">
 				<body className={inter.className}>
-					{" "}
+					<ToasterProvider />
 					<ModalProvider />
 					{children}
 				</body>

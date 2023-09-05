df049f2992ad0b78e26e36c5da3252ee3e52eadf
abishek-as <abeshek.as@gmail.com>
2023-09-05 10:53:36 +0530

    Dashboard setup (admin)

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -40,7 +40,7 @@ export const StoreModal = () => {
 		try {
 			setLoading(true);
 			const response = await axios.post("/api/stores", values);
-			toast.success("Store created.");
+			window.location.assign(`/${response.data.id}`);
 		} catch (error) {
 			toast.error("Something went wrong");
 		} finally {
